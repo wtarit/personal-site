99dd2fff865f56078cf833973868ac16da8e9197
wtarit <60599564+wtarit@users.noreply.github.com>
2025-01-22 19:14:49 -0800

    Update WorkExperiences, TechnicalExtracurriculars

--- app/components/TechnicalExtracurriculars.tsx
+++ app/components/TechnicalExtracurriculars.tsx
@@ -6,40 +6,57 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 
+interface Extracurricular {
+  title: string;
+  organization: string;
+  duration: string;
+  descriptions: string[];
+}
+
+const technicalExtracurriculars: Extracurricular[] = [
+  {
+    title: "Software Developer",
+    organization: "UBC Uncrewed Aircraft Systems Engineering Design Team",
+    duration: "September 2023 - Present",
+    descriptions: [
+      "Contributed to the development of autonomous drones competing in the the Aerial Evolution Association of Canada 2024 competition, achieving 2nd place in the proposals phase and 3rd place in the demonstration phase",
+      "Successfully migrated 15+ endpoints in MissionPlanner-Scripts, a REST APIs and WebSocket interface between a backend system and ArduPilot, to a cross-platform architecture using Pymavlink and Flask, eliminating reliance on Windows-only MissionPlanner client scripts",
+      "Crafted OpenAPI Specifications and Postman collections to facilitate team collaboration and API integration",
+      "Developed a pipeline for drone target detection using a fine-tuned YOLOv8 model with tiled inference",
+      "Created dataset generator program using OpenCV and imgaug, eliminating the need for manual data labeling",
+    ],
+  },
+];
+
 export default function TechnicalExtracurriculars() {
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl py-6">
         Technical Extracurriculars
       </h1>
-      <Card>
-        <CardHeader>
-          <CardTitle>Software Developer</CardTitle>
-          <CardDescription className="flex justify-between flex-col md:flex-row">
-            <div>UBC Uncrewed Aircraft Systems Engineering Design Team</div>
-            <div>September 2023 - Present</div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ul className="list-disc px-6">
-            <li>
-              Developed Object Detection, Classification, and Localization
-              software for finding targets from a drone with a team of four
-            </li>
-            <li>
-              Reduced time to create training dataset by implementing dataset
-              generator using OpenCV and imgaug
-            </li>
-            <li>
-              Created inference pipeline integrating tiled inference, object
-              detection, and OCR for processing captured image
-            </li>
-          </ul>
-        </CardContent>
-        {/* <CardFooter>
+      {technicalExtracurriculars.map((extracurricular, index) => {
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>{extracurricular.title}</CardTitle>
+              <CardDescription className="flex justify-between flex-col md:flex-row">
+                <div>{extracurricular.organization}</div>
+                <div>{extracurricular.organization}</div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="list-disc px-6">
+                {extracurricular.descriptions.map((des, index) => {
+                  return <li key={index}>{des}</li>;
+                })}
+              </ul>
+            </CardContent>
+            {/* <CardFooter>
           <p>Card Footer</p>
         </CardFooter> */}
-      </Card>
+          </Card>
+        );
+      })}
     </div>
   );
 }
--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -6,47 +6,57 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 
+interface WorkExperience {
+  title: string;
+  company: string;
+  duration: string;
+  descriptions: string[];
+}
+
+const workExperiences: WorkExperience[] = [
+  {
+    title: "Software Developer Intern",
+    company: "Applicad Public Company Limited - Bangkok, Thailand",
+    duration: "June - August 2024",
+    descriptions: [
+      "Conducted an analysis of the sales team's needs for a customer identification solution, defining essential functionalities and designing user flows in collaboration with a UX/UI designer to develop a Figma prototype",
+      "Developed a real-time facial recognition web application using Next.js, featuring a user interface for customer identification with 95% accuracy via webcam, along with an admin page for managing customer data",
+      "Built a backend service in Python with FastAPI for face vector comparison using ChromaDB, integrated with a PostgreSQL database for storing User Data, and linked it to the existing internal customer database",
+      "Implemented a robust authentication system featuring role-based access control with NextAuth, utilizing JWT sessions and email OTP for secure login, and employed Next.js middleware for token verification",
+      "Deployed the application and databases to an on-premises Ubuntu server using Docker and Docker Compose",
+    ],
+  },
+];
+
 export default function WorkExperience() {
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl py-6">
         Work Experience
       </h1>
-      <Card>
-        <CardHeader>
-          <CardTitle>Software Developer Intern</CardTitle>
-          <CardDescription className="flex justify-between flex-col md:flex-row">
-            <div>Applicad Public Company Limited - Bangkok, Thailand</div><div>June - August 2024</div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ul className="list-disc px-6">
-            <li>
-              Developed a full-stack real-time face recognition web application
-              using Next.js, enabling customer identification
-            </li>
-            <li>
-              Built a backend service in Python with FastAPI for face vector
-              comparison using ChromaDB, integrated with a PostgreSQL database
-              for storing User Data, and linked it to the existing internal
-              customer database
-            </li>
-            <li>
-              Implemented an Email OTP authentication system using NextAuth
-              Credentials Provider, with a custom Python backend for email
-              delivery via SMTP and credential validation and utilized Next.js
-              middleware for role separation
-            </li>
-            <li>
-              Deployed the application on an on-premises Ubuntu server using
-              Docker and Docker Compose
-            </li>
-          </ul>
-        </CardContent>
-        {/* <CardFooter>
+      {workExperiences.map((workExperience, index) => {
+        return (
+          <Card key={index}>
+            <CardHeader>
+              <CardTitle>{workExperience.title}</CardTitle>
+              <CardDescription className="flex justify-between flex-col md:flex-row">
+                <div>{workExperience.company}</div>
+                <div>{workExperience.duration}</div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="list-disc px-6">
+                {workExperience.descriptions.map((des, index) => {
+                  return <li key={index}>{des}</li>;
+                })}
+              </ul>
+            </CardContent>
+            {/* <CardFooter>
           <p>Card Footer</p>
         </CardFooter> */}
-      </Card>
+          </Card>
+        );
+      })}
     </div>
   );
 }
